9b93f14336b9697339dfb451afc488c739a91b36
Ayem-Dan <danielMMomodu@gmail.com>
2023-06-23 20:16:34 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import nodemon from "nodemon";
+
+
+/**CONFIGURATIONS */
